66eeb19317b9e2b0fdb18b138375f4d43a90ed75
Tyler Latshaw <tylerlatshaw@gmail.com>
2026-06-15 19:28:10 -0400

    Updated camera switch

--- src/components/scan/scanner-container.tsx
+++ src/components/scan/scanner-container.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { sanitizeFilamentId } from "@/utilities/scan-functions";
-import { Scanner } from "@yudiel/react-qr-scanner";
+import { Scanner, useDevices } from "@yudiel/react-qr-scanner";
+import { SwitchCameraIcon } from "lucide-react";
 import { useState } from "react";
+import { Button } from "../ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 type Props = {
     onFilamentIdSubmit: (filamentId: string) => void;
@@ -12,9 +15,11 @@ export default function ScannerContainer({
     onFilamentIdSubmit,
 }: Props) {
     const [error, setError] = useState("");
+    const devices = useDevices();
+    const [selectedDevice, setSelectedDevice] = useState<MediaDeviceInfo>();
 
     return (
-        <div className="flex flex-col items-center justify-center gap-2">
+        <div className="relative flex flex-col items-center justify-center gap-2">
             <Scanner
                 onScan={(result) => {
                     const rawValue = result[0]?.rawValue;
@@ -42,18 +47,49 @@ export default function ScannerContainer({
                     "qr_code_model_2",
                 ]}
                 classNames={{
-                    container: "rounded-lg",
+                    container: "peer rounded-lg"
                 }}
                 sound={false}
                 allowMultiple={false}
+                constraints={{
+                    deviceId: selectedDevice?.deviceId
+                }}
             />
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant={"default"}
+                        size={"icon"}
+                        className="absolute z-50 top-3 right-3"
+                    >
+                        <SwitchCameraIcon className="p-2 bg-primary shadow rounded-full size-10" />
+                    </Button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className="bg-background border border-accent text-nowrap w-fit">
+                    <DropdownMenuGroup>
+                        {
+                            devices.map((device) => {
+                                return <DropdownMenuItem
+                                    key={device.deviceId}
+                                    onClick={() => setSelectedDevice(device)}
+                                    className="w-fit"
+                                >
+                                    {device.label}
+                                </DropdownMenuItem>
+                            })
+                        }
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
 
             <span className="mt-2 text-base text-foreground">
                 Scan the spool&apos;s QR code
             </span>
 
             {error && (
-                <span className="text-danger">
+                <span className="text-danger text-center">
                     {error}
                 </span>
             )}
